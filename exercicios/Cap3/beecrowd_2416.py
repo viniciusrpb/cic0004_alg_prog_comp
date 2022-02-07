d3ab77f8307bfdf31249901a53e6a659623df171
Vinicius R. P. Borges <viniciusrpb@gmail.com>
2022-02-07 19:34:31 -0300

    Add files via upload

--- exercicios/Cap3/beecrowd_2416.py
+++ exercicios/Cap3/beecrowd_2416.py
@@ -1,3 +1,16 @@
+'''
+Universidade de Brasília
+Departamento de Ciência da Computação
+CIC0004 - Algoritmos e Programação de Computadores
+Prof. Dr. Vinícius R. P. Borges
+ 
+Tópico: Algoritmos Sequenciais (Python)
+Objetivo: Solução do problema beecrowd 2416 - Corrida (OBI - Olimpíada Brasileira de Informática 2012)
+          https://www.beecrowd.com.br/judge/en/problems/view/2416
+ 
+Linha de comando para executar: python3 beecrowd_2416.py
+'''
+
 c,n = input().split()
 c = int(c)
 n = int(n)
